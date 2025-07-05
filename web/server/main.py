c7e2aecd4f3282a879df6d05e9a32e879cc3a566
yabamiah <211063254@aluno.unb.br>
2025-07-05 15:52:45 -0300

    feat(docker): adicição do frontend and backend no docker-compose e atualização do Dockerfiles

--- web/server/main.py
+++ web/server/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT', 'localhost:9000')
+MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT', 'minio:9000')
 MINIO_ACCESS_KEY = os.getenv('MINIO_ACCESS_KEY', 'minioadmin')
 MINIO_SECRET_KEY = os.getenv('MINIO_SECRET_KEY', 'minioadmin')
 MINIO_BUCKET = os.getenv('MINIO_BUCKET', 'scraped-articles')
