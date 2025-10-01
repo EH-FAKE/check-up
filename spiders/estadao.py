eda94036122dc1bb77c6319fe10adbfd12b55f98
GabrielSMonteiro <gabrielmonteiro1203@gmail.com>
2025-10-01 15:23:57 -0300

    Fix: Corrige o Scrapy do Estadão
    Closes #42

--- spiders/estadao.py
+++ spiders/estadao.py
@@ -1,13 +1,19 @@
 import scrapy
-
+from scrapy.http import Request
 from spiders.base import BaseSpider
 from spiders.items import URLItem
-
 
 class EstadaoSpider(BaseSpider):
     name = "estadaospider"
     start_urls = ["https://www.estadao.com.br/"]
     allowed_domains = ["estadao.com.br"]
+    
+    custom_settings = {
+        'DOWNLOAD_DELAY': 2,
+        'RANDOMIZE_DOWNLOAD_DELAY': True,
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'ROBOTSTXT_OBEY': True,
+    }
 
     def allow_url(self, entry_url):
         # TODO: remover www.estadao.com.br/recomenda/
